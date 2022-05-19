e3c4bd9df89c97537b8d0a53c5e716ff905b8cbd
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 18:28:01 -0500

    Mi primer Hola Mundo en Java

---
+++ src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo, Bienvenidos a Java"); //Esto es un saludo inicial
+
+        /* Esto es un comentario de
+           multiples líneas para los casos
+           donde yo necesito dejar bien explicado algo
+         */
+
+    }
+}
